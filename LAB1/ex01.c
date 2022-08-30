69b23d4fac46f49615b19833ffe395456185622a
JoãoVictor <jvoamorim@gmail.com>
2022-08-29 21:53:09 -0300

    21:53

--- LAB1/ex01.c
+++ LAB1/ex01.c
@@ -13,16 +13,40 @@ struct aluno
 typedef struct aluno Aluno;
 
 int main(void){
-    Aluno* vAlun = (Aluno*)malloc(sizeof(Aluno)*QTD);
-    insereDados(vAlun[1],2111295,"joao victor","rua umdois","219999222");
-    insereDados(vAlun[2],2111295,"joao victor","rua umdois","219999222");
+    int i;
+    char nome[] = "jose carlos";
+    // Aluno** vAlun = (Aluno**)malloc(QTD*sizeof(Aluno*));
+    //  for (i=0;i<QTD;i++){
+    //      vAlun[i]=(Aluno*)malloc(sizeof(Aluno));
+
+    //  }
+     int tam = tamStr(nome);
+     printf(tam);
+     imprimeStr(nome)
+    //vAlun[0]=(Aluno*)malloc(sizeof(Aluno));
+    //insereDados(vAlun[0],2111295,"joao victor","rua umdois","219999222");
+    //insereDados(vAlun[1],0011295,"joao owsei","rua doisum","219999222");
 
     return 0;
 }
 
-void insereDados(Aluno* vAlun,int mtr,char *nome,char*ender,char*tel){
+void insereDados(Aluno** vAlun,int i,int mtr,char *nome,char*ender,char*tel){
+    
     vAlun->mtr = mtr;
     strcpy(vAlun->nome,nome);
     strcpy(vAlun->ender,ender);
     strcpy(vAlun->tel,tel);
+}
+
+int tamStr(char string[]){
+    if (string[0] =='\0')
+        return 0;
+    return 1+tamStr(&string[1]);
+}
+void imprimeStr(char string[]){
+    if(string[0]=='\0')
+        return;
+    printf('%c',string[0]);
+    return string[0]+1;
+
 }
