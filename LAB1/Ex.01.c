a636f74656e555f356a7e3fcd7d6ce823b64586d
c2111295 <c2111295@PEIXE>
2022-09-05 16:41:19 -0300

    16:41

--- LAB1/Ex.01.c
+++ LAB1/Ex.01.c
@@ -1,5 +1,5 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #define QTD 5
 
@@ -7,47 +7,49 @@ struct aluno
 {
     int mtr;
     char nome[81];
-    char end[121];
-    char num[21];
+    char ender[21];
+    char tel[21];
 };
 typedef struct aluno Aluno;
 
+void insereDados(Aluno* vAlun,int mtr,char *nome,char *ender,char *tel){
+    
+    vAlun->mtr = mtr;
+    strcpy(vAlun->nome,nome);
+    strcpy(vAlun->ender,ender);
+    strcpy(vAlun->tel,tel);
+}
+
+int tamStr(char string[]){
+    if (string[0] =='\0')
+        return 0;
+    return 1+tamStr(&string[1]);
+}
+void imprimeStr(char string[]){
+    if(string[0]=='\0')
+        return;
+    printf('%c',string[0]);
+    return imprimeStr(string+1);
+
+}
+
 int main(void){
     int i;
-    Aluno* vAlun = (Aluno*)malloc(sizeof(Aluno)*QTD);
-    Aluno** vAlun[QTD];
+    char nome[] = "jose carlos";
+    Aluno** vAlun = (Aluno**)malloc(QTD*sizeof(Aluno*));
     for (i=0;i<QTD;i++){
-        vAlun[i] = *(vAlun[i]);
+        vAlun[i]=(Aluno*)malloc(sizeof(Aluno));
+
     }
-    //insereDados(vAlun,2111295,"joao victor","barao do flamengo","21969214705");
-    
-
-
+    insereDados(vAlun[0],005421,"jose carlos","rua doisum","215456487");
+    insereDados(vAlun[1],2111295,"joao victor","rua umdois","219999222");
+    insereDados(vAlun[2],2111295,"pedro lucas","rua tresquatro","214842512");
+    insereDados(vAlun[3],2111295,"jullia","rua cincoseis","215456425");
+    insereDados(vAlun[4],2111295,"luciana","rua seteoito","216545452");
+      
+      
+    int tam = tamStr(nome);
+    printf(tam);
+    imprimeStr(nome);
     return 0;
 }
-
-/*Aluno* criaVet(){
-    Aluno* vAlunos = (Aluno*)malloc(sizeof(Aluno*));
-    return vAlunos;
-}*/
-void insereDados(Aluno* vAlun,int mtr,char*nome,char* end,char* num){
-    vAlun->mtr=mtr;
-    strcpy(vAlun->nome,nome);
-    strcpy(vAlun->end,end);
-    strcpy(vAlun->num,num);
-
-}
-
-
-/*
-int main() {
-  
-  Aluno* struc = malloc(sizeof(Aluno));
-  //Aluno* str[1];
-  Aluno** vAlun[5];
-  vAlun[0] = struc;
-  vAlun[0].mtr = 100;
-  //str[0]= malloc(sizeof(Aluno));
-  return 0;
-}
-*/
